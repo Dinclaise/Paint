fc680cbe233c414b4491cea167f8c74a67010b9b
Anton <Anohin.Anton@wildberries.ru>
2021-10-05 22:19:41 +0300

    added png of toolbar buttons

---
+++ paint-front/src/components/SettingsBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../styles/settings-bar.scss";
+
+const SettingsBar = (props) => {
+  return <div className={"settings-bar"}>settings bar</div>;
+};
+
+export default SettingsBar;
