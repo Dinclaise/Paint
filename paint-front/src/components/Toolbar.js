5acb893c2c40809f6b46755a0fa34a893496367b
Anton <Anohin.Anton@wildberries.ru>
2021-10-10 21:19:16 +0300

    feat/ added Eraser, Circle, Rect

--- paint-front/src/components/Toolbar.js
+++ paint-front/src/components/Toolbar.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import canvasState from "../store/canvasState";
+import toolState from "../store/toolState";
 
 import "../styles/toolbar.scss";
+import Brush from "./../tools/Brush";
+import Rect from "./../tools/Rect";
+import Circle from "./../tools/Circle";
 
 const Toolbar = (props) => {
+  const [filled, setFilled] = useState(false);
+
+  const handleChooseBrush = () => {
+    return toolState.setTool(new Brush(canvasState.canvas));
+  };
+
+  const handleChooseRect = () => {
+    return toolState.setTool(new Rect(canvasState.canvas));
+  };
+
+  const handleChooseCircle = () => {
+    return toolState.setTool(new Circle(canvasState.canvas));
+  };
+
   return (
     <div className={"toolbar"}>
-      <button className={"toolbar__btn brush"} />
-      <button className={"toolbar__btn rect"} />
-      <button className={"toolbar__btn circle"} />
+      <button className={"toolbar__btn brush"} onClick={handleChooseBrush} />
+      <button className={"toolbar__btn rect"} onClick={handleChooseRect} />
+      <button className={"toolbar__btn circle"} onClick={handleChooseCircle} />
       <button className={"toolbar__btn eraser"} />
       <button className={"toolbar__btn line"} />
       {/* <button className={"toolbar__btn palitra"} /> */}
